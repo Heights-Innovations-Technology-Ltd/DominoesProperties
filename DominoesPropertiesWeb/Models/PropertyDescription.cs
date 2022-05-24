29e0910c2d671ae9bab2d751a12f8ff5e34cfd52
Habeeb Bello <habeeb@nestinvestco.com>
2022-05-24 02:47:39 +0100

    fixed

--- DominoesPropertiesWeb/Models/PropertyDescription.cs
+++ DominoesPropertiesWeb/Models/PropertyDescription.cs
@@ -2,5 +2,19 @@
 {
     public class PropertyDescription
     {
+        public int? Bathroom { get; set; }
+        public int? Toilet { get; set; }
+        public int? FloorLevel { get; set; }
+        public int? Bedroom { get; set; }
+        public string LandSize { get; set; }
+        public bool? AirConditioned { get; set; }
+        public bool? Refrigerator { get; set; }
+        public bool? Parking { get; set; }
+        public bool? SwimmingPool { get; set; }
+        public bool? Laundry { get; set; }
+        public bool? Gym { get; set; }
+        public bool? SecurityGuard { get; set; }
+        public bool? Fireplace { get; set; }
+        public bool? Basement { get; set; }
     }
 }
