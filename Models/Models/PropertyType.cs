adcda4048cebbe9d5c2a039bcb4b7036825f5d86
Habeeb Bello <habeeb@nestinvestco.com>
2022-06-01 14:42:00 +0100

    kip

--- Models/Models/PropertyType.cs
+++ Models/Models/PropertyType.cs
@@ -13,7 +13,7 @@ namespace Models.Models
             Properties = new HashSet<Property>();
         }
 
-        //[IgnoreDataMember]
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime? DateCreated { get; set; }
