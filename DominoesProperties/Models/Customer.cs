d4229951e9e49757b75c60fc5760efc4d21890f9
aojinadu <ayo02034reel@yahoo.com>
2022-10-21 19:08:11 +0100

    Password pattern fix

--- DominoesProperties/Models/Customer.cs
+++ DominoesProperties/Models/Customer.cs
@@ -7,13 +7,13 @@ namespace DominoesProperties.Models
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Password cannot be less than 8 characters")]
         [MaxLength(16, ErrorMessage = "Password cannot be more than 16 characters")]
-        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Password does not match specified pattern. \nPassword must contain atleast <html><ul><li>one uppercase</li><li>one lowercase</li> <li>one number</li><li>one special character</li></ul></html>")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Password does not match specified pattern. \nPassword must contain atleast <html><ul><li>one uppercase</li><li>one lowercase</li> <li>one number</li><li>one of the following special characters @$!%*#?&</li></ul></html>")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Confirm password is required")]
         [MinLength(8, ErrorMessage = "Password cannot be less than 8 characters")]
         [MaxLength(16, ErrorMessage = "Password cannot be more than 16 characters")]
         [Compare("Password", ErrorMessage = "Password and Confirm password does not match")]
-        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Confirm password does not match specified pattern. \nConfirm password must contain atleast <html><ul><li>one uppercase</li><li>one lowercase</li> <li>one number</li><li>one special character</li></ul></html>")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Confirm password does not match specified pattern. \nConfirm password must contain atleast <html><ul><li>one uppercase</li><li>one lowercase</li> <li>one number</li><li>one of the following special characters @$!%*#?&</li></ul></html>")]
         public string ConfirmPassword { get; set; }
         [Required(ErrorMessage = "Firstname is required")]
         [MaxLength(50, ErrorMessage = "Firstname cannot be more than 50 characters")]
