4972e2471bae1dec089fa9eb44c226f1bb06f0a4
Ayotola Jinadu <ayotolaj@growwithnest.com>
2023-02-17 00:49:43 -0400

    blog inclusive
    Signed-off-by: Ayotola Jinadu <ayotolaj@growwithnest.com>

--- DominoesPropertiesWeb/Controllers/PropertyController.cs
+++ DominoesPropertiesWeb/Controllers/PropertyController.cs
@@ -1,4 +1,9 @@
-﻿using DominoesPropertiesWeb.HttpContext;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Threading.Tasks;
+using DominoesPropertiesWeb.HttpContext;
 using DominoesPropertiesWeb.Models;
 using DominoesPropertiesWeb.Repository;
 using Microsoft.AspNetCore.Hosting;
@@ -7,29 +12,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
 namespace DominoesPropertiesWeb.Controllers
 {
     public class PropertyController : Controller
     {
         private readonly IConfiguration _config;
-        private readonly ISession session;
-
-        private readonly IHttpContext httpContext;
         private readonly IUploadsRepository _uploadRepository;
         private readonly IWebHostEnvironment hostEnvironment;
 
-        string url = string.Empty;
+        private readonly IHttpContext httpContext;
+        private readonly ISession session;
         dynamic jsonObj = new JObject();
 
-        public PropertyController(IConfiguration config, IHttpContextAccessor httpContextAccessor, IHttpContext httpContext, IWebHostEnvironment hostEnvironment, IUploadsRepository uploadRepository)
+        string url = string.Empty;
+
+        public PropertyController(IConfiguration config, IHttpContextAccessor httpContextAccessor,
+            IHttpContext httpContext, IWebHostEnvironment hostEnvironment, IUploadsRepository uploadRepository)
         {
             _config = config;
             this.session = httpContextAccessor.HttpContext.Session;
@@ -38,6 +37,7 @@ namespace DominoesPropertiesWeb.Controllers
             url = _config["Base_URL"];
             _uploadRepository = uploadRepository;
         }
+
         public IActionResult Index()
         {
             var isAuthAdmin = this.session.GetString("RoleFK");
@@ -45,6 +45,7 @@ namespace DominoesPropertiesWeb.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+
             return View();
         }
 
@@ -55,6 +56,7 @@ namespace DominoesPropertiesWeb.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+
             return View();
         }
 
@@ -66,6 +68,7 @@ namespace DominoesPropertiesWeb.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+
             return View();
         }
 
@@ -77,6 +80,7 @@ namespace DominoesPropertiesWeb.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+
             return View();
         }
 
@@ -84,7 +88,7 @@ namespace DominoesPropertiesWeb.Controllers
         public async Task<JsonResult> CreateProperty([FromBody] dynamic property)
         {
             JObject jObject = JsonConvert.DeserializeObject<JObject>(Convert.ToString(property));
-            
+
             dynamic obj = new ExpandoObject();
             dynamic DesObj = new ExpandoObject();
 
@@ -130,12 +134,12 @@ namespace DominoesPropertiesWeb.Controllers
             var data = await res.GetAwaiter().GetResult();
             return Json(JsonConvert.SerializeObject(data));
         }
-        
+
         [Route("update-property/{propertyId}")]
         public async Task<JsonResult> EditProperty([FromBody] dynamic property, string propertyId)
         {
             JObject jObject = JsonConvert.DeserializeObject<JObject>(Convert.ToString(property));
-            
+
             dynamic obj = new ExpandoObject();
 
             obj.Location = Convert.ToString(jObject["Location"]);
@@ -158,7 +162,7 @@ namespace DominoesPropertiesWeb.Controllers
             var data = await res.GetAwaiter().GetResult();
             return Json(JsonConvert.SerializeObject(data));
         }
-        
+
         [Route("property-decription/{propertyId}")]
         public async Task<JsonResult> PropertyDescription([FromBody] dynamic property, string propertyId)
         {
@@ -203,7 +207,7 @@ namespace DominoesPropertiesWeb.Controllers
             var data = res.Status == TaskStatus.RanToCompletion ? res.Result : null;
             return Json(JsonConvert.SerializeObject(data));
         }
-        
+
         [Route("single-property/{uniqueId}")]
         public async Task<JsonResult> GetSingleProperty([FromRoute] string uniqueId)
         {
@@ -220,25 +224,26 @@ namespace DominoesPropertiesWeb.Controllers
             var query = new Dictionary<string, string>();
 
             JObject jObject = JsonConvert.DeserializeObject<JObject>(Convert.ToString(filter));
-            dynamic DesObj = new ExpandoObject();
-            var propertyFilter = new PropertyFilter();
-            //propertyFilter.Category = Convert.ToInt32(jObject["Category"]);
-            propertyFilter.Bathroom = Convert.ToInt32(jObject["Bathroom"]);
-            propertyFilter.Toilet = Convert.ToInt32(jObject["Toilet"]);
-            propertyFilter.Floor = Convert.ToInt32(jObject["FloorLevel"]);
-            propertyFilter.Bedroom = Convert.ToInt32(jObject["Bedroom"]);
-            propertyFilter.MinPrice = Convert.ToDecimal(jObject["MinPrice"]);
-            propertyFilter.MaxPrice = Convert.ToDecimal(jObject["MaxPrice"]);
-            propertyFilter.AirConditioned = Convert.ToBoolean(jObject["AirConditioned"]);
-            propertyFilter.Refridgerator = Convert.ToBoolean(jObject["Refrigerator"]);
-            propertyFilter.Parking = Convert.ToBoolean(jObject["Parking"]);
-            propertyFilter.SwimmingPool = Convert.ToBoolean(jObject["SwimmingPool"]);
-            propertyFilter.Laundry = Convert.ToBoolean(jObject["Laundry"]);
-            propertyFilter.Gym = Convert.ToBoolean(jObject["Gym"]);
-            propertyFilter.SecurityGuard = Convert.ToBoolean(jObject["SecurityGuard"]);
-            propertyFilter.Fireplace = Convert.ToBoolean(jObject["Fireplace"]);
-            propertyFilter.Basement = Convert.ToBoolean(jObject["Basement"]);
-            propertyFilter.Location = Convert.ToString(jObject["Location"]);
+            var propertyFilter = new PropertyFilter
+            {
+                //propertyFilter.Category = Convert.ToInt32(jObject["Category"]);
+                Bathroom = Convert.ToInt32(jObject["Bathroom"]),
+                Toilet = Convert.ToInt32(jObject["Toilet"]),
+                Floor = Convert.ToInt32(jObject["FloorLevel"]),
+                Bedroom = Convert.ToInt32(jObject["Bedroom"]),
+                MinPrice = Convert.ToDecimal(jObject["MinPrice"]),
+                MaxPrice = Convert.ToDecimal(jObject["MaxPrice"]),
+                AirConditioned = Convert.ToBoolean(jObject["AirConditioned"]),
+                Refridgerator = Convert.ToBoolean(jObject["Refrigerator"]),
+                Parking = Convert.ToBoolean(jObject["Parking"]),
+                SwimmingPool = Convert.ToBoolean(jObject["SwimmingPool"]),
+                Laundry = Convert.ToBoolean(jObject["Laundry"]),
+                Gym = Convert.ToBoolean(jObject["Gym"]),
+                SecurityGuard = Convert.ToBoolean(jObject["SecurityGuard"]),
+                Fireplace = Convert.ToBoolean(jObject["Fireplace"]),
+                Basement = Convert.ToBoolean(jObject["Basement"]),
+                Location = Convert.ToString(jObject["Location"])
+            };
 
             //if (Convert.ToInt32(jObject["Category"]) > 0) query.Add("Category", propertyFilter.Category.ToString());
             if (Convert.ToInt32(jObject["Bathroom"]) > 0) query.Add("Bathroom", propertyFilter.Bathroom.ToString());
@@ -247,16 +252,21 @@ namespace DominoesPropertiesWeb.Controllers
             if (Convert.ToInt32(jObject["Bedroom"]) > 0) query.Add("Bedroom", propertyFilter.Bedroom.ToString());
             if (Convert.ToDecimal(jObject["MinPrice"]) > 0) query.Add("MinPrice", propertyFilter.MinPrice.ToString());
             if (Convert.ToDecimal(jObject["MaxPrice"]) > 0) query.Add("MaxPrice", propertyFilter.MaxPrice.ToString());
-            if (Convert.ToBoolean(jObject["AirConditioned"])) query.Add("AirConditioned", propertyFilter.AirConditioned.ToString());
-            if (Convert.ToBoolean(jObject["Refrigerator"])) query.Add("Refridgerator", propertyFilter.Refridgerator.ToString());
+            if (Convert.ToBoolean(jObject["AirConditioned"]))
+                query.Add("AirConditioned", propertyFilter.AirConditioned.ToString());
+            if (Convert.ToBoolean(jObject["Refrigerator"]))
+                query.Add("Refridgerator", propertyFilter.Refridgerator.ToString());
             if (Convert.ToBoolean(jObject["Parking"])) query.Add("Parking", propertyFilter.Parking.ToString());
-            if (Convert.ToBoolean(jObject["SwimmingPool"])) query.Add("SwimmingPool", propertyFilter.SwimmingPool.ToString());
+            if (Convert.ToBoolean(jObject["SwimmingPool"]))
+                query.Add("SwimmingPool", propertyFilter.SwimmingPool.ToString());
             if (Convert.ToBoolean(jObject["Laundry"])) query.Add("Laundry", propertyFilter.Laundry.ToString());
             if (Convert.ToBoolean(jObject["Gym"])) query.Add("Gym", propertyFilter.Gym.ToString());
-            if (Convert.ToBoolean(jObject["SecurityGuard"])) query.Add("SecurityGuard", propertyFilter.SecurityGuard.ToString());
+            if (Convert.ToBoolean(jObject["SecurityGuard"]))
+                query.Add("SecurityGuard", propertyFilter.SecurityGuard.ToString());
             if (Convert.ToBoolean(jObject["Fireplace"])) query.Add("Fireplace", propertyFilter.Fireplace.ToString());
             if (Convert.ToBoolean(jObject["Basement"])) query.Add("Basement", propertyFilter.Basement.ToString());
-            if (Convert.ToString(jObject["Location"]).Length > 0) query.Add("Location", propertyFilter.Location.ToString());
+            if (Convert.ToString(jObject["Location"]).Length > 0)
+                query.Add("Location", propertyFilter.Location.ToString());
 
             var res = Task.Run(() => httpContext.Get("Property", query));
             await Task.WhenAll(res);
@@ -277,6 +287,7 @@ namespace DominoesPropertiesWeb.Controllers
                 foreach (var file in json)
                 {
                     #region old implementation
+
                     //var fileContent = new StreamContent(Request.Body);
                     //fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(file.ContentType);
                     //fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
@@ -286,6 +297,7 @@ namespace DominoesPropertiesWeb.Controllers
                     //    //UploadType = Convert.ToInt32(jObject[""])
                     //};
                     //content.Add(fileContent);
+
                     #endregion
 
                     var upload = new PropertyFileUpload
@@ -294,8 +306,8 @@ namespace DominoesPropertiesWeb.Controllers
                         UploadType = (UploadType)Convert.ToInt32(jObject["uploadType"])
                     };
                     fileUploads.Add(upload);
-
                 }
+
                 var uploadResponse = _uploadRepository.UploadPropertyImages(fileUploads, propertyId, Request);
 
                 var res = Task.Run(() => httpContext.Post("Property/uploads/" + propertyId, uploadResponse));
@@ -304,6 +316,7 @@ namespace DominoesPropertiesWeb.Controllers
                 var data = res.Status == TaskStatus.RanToCompletion ? res.Result : null;
                 return Json(JsonConvert.SerializeObject(data));
             }
+
             return Json(this.StatusCode(StatusCodes.Status204NoContent, "Empty request body"));
         }
 
@@ -315,17 +328,21 @@ namespace DominoesPropertiesWeb.Controllers
             {
                 if (formfile.Length > 0)
                 {
-                    string filePath = Path.Combine(hostEnvironment.WebRootPath, _config["app_settings:commodityUploads"]);
+                    string filePath = Path.Combine(hostEnvironment.WebRootPath,
+                        _config["app_settings:commodityUploads"]);
                     if (!Directory.Exists(filePath))
                         Directory.CreateDirectory(filePath);
 
-                    using (var stream = new FileStream(Path.Combine(filePath, propertyId + formfile.FileName), FileMode.Create))
+                    using (var stream = new FileStream(Path.Combine(filePath, propertyId + formfile.FileName),
+                               FileMode.Create))
                     {
                         formfile.CopyTo(stream);
                     }
+
                     filenamelistofCustomerFiles.Add(formfile.FileName);
                 }
             }
+
             return filenamelistofCustomerFiles;
         }
 
